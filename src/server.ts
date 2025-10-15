ecb2f0fe88355258362d6a933f1626478f7bae42
Samsegun <oyebadesegunsam@gmail.com>
2025-10-15 18:47:55 +0100

    inital commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import http from "http";
+import app from "./app";
+
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`);
+});
